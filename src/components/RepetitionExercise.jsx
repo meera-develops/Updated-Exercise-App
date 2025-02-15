d5e9762fbb1158bcebaed10963ec191faa857771
Meera Bhola <meerabhola3@gmail.com>
2025-02-15 01:57:28 -0500

    included counter ability

--- src/components/RepetitionExercise.jsx
+++ src/components/RepetitionExercise.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Repetition.css';
 
 function Screen1({ exerciseName }) {
+
+    const [count, setCount] = useState(0);
+
+    const increaseCount = () => {
+        setCount(count + 1);
+    }
+    
+    const decreaseCount = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
+    const resetCount = () => {
+        setCount(0);
+    }
+
     return (
         <>
             <div className='main-container'>
                 <h1>EXERCISE HERO</h1>
                 <h1>{exerciseName}</h1>
-                {/* code in counter */}
+                <h2 style={{ fontSize: '4rem'} }>{count}</h2>
+                <div className='button-container'>
+                    <button className='my-button' onClick={increaseCount}>
+                        +1
+                    </button>
+                    <button className='my-button' onClick={decreaseCount}>
+                        -1
+                    </button>
+                    <button onClick={resetCount}>
+                        Reset
+                    </button>
+                </div>
             </div>
-        
         </>
     )
 }
