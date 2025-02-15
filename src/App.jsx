f359081bf7ce8e5f48d1ca9a6b7fce34b7fca9d8
Meera Bhola <meerabhola3@gmail.com>
2025-02-14 21:37:03 -0500

    displays screen underneath buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import { useState } from 'react'
-
+import React, { useState } from 'react'
 import './App.css'
+import pushups from './assets/img/pushups-pic.png';
+import planks from './assets/img/planks.png';
+import running from './assets/img/running.png';
+import Screen1 from './components/screen1';
+
 
 function App() {
-  //const [count, setCount] = useState(0)
+
+  const [curScreen, setCurScreen] = useState(null);
+
+  const handleNavigation = (screen) => {
+    setCurScreen(screen);
+  };
 
   return (
     <>
-      <div>
-        <h1>EXERCISE HERO</h1>        
-      </div>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        
+      <div className='main-container'>
+        <h1>EXERCISE HERO</h1> 
+
+        <div className='images'>
+          <div className='image-item'>
+            <img src={pushups} alt="Image of woman in the beginning of push-up position" />
+              <button className='text-buttons' onClick={() => handleNavigation('Screen1')}>
+                Push-ups
+              </button>
+          </div>
+          <div className='image-item'>
+            <img src={running} alt="Image of man running outside" />
+            <button className='text-buttons'>
+              Running
+            </button>
+          </div>
+          <div className='image-item'>
+            <img src={planks} alt="Image of woman holding a plank" /> 
+            <button className='text-buttons'>
+              Planks
+            </button>
+          </div>
+        </div> 
+
+        <div className='screen-display'>
+          {curScreen === 'Screen1' && <Screen1 />}
+        </div>
+
+
       </div>
       
     </>
---
+++ src/components/Screen1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Screen1() {
+    return (
+        <>
+            <div>
+                <h1>Welcome to the pushups screen</h1>
+            </div>
+        
+        </>
+    )
+}
+
+export default Screen1;
